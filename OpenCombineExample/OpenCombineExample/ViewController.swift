d0a980e0eac1b9476de628780e898a5da66a6e44
liuguoqiang <janstilau@gmail.com>
2024-01-09 00:30:06 +0800

    mod: run example

---
+++ OpenCombineExample/OpenCombineExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenCombineExample
+//
+//  Created by liuguoqiang on 2024/1/9.
+//
+
+import UIKit
+import OpenCombine
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
