5041b47bd79bf13ec22f3da419da1be59054e02f
liuguoqiang <janstilau@gmail.com>
2024-04-02 16:03:00 +0800

    mod: sync

--- comb-materials-editions-4.0/17-schedulers/projects/Final.playground/Pages/subscribeOn-receiveOn.xcplaygroundpage/Contents.swift
+++ comb-materials-editions-4.0/17-schedulers/projects/Final.playground/Pages/subscribeOn-receiveOn.xcplaygroundpage/Contents.swift
@@ -15,28 +15,28 @@ let currentThread = Thread.current.number
 print("Start computation publisher on thread \(currentThread)")
 
 let subscription = computationPublisher
-  .subscribe(on: queue)
-  .receive(on: DispatchQueue.main)
-  .sink { value in
-    let thread = Thread.current.number
-    print("Received computation result on thread \(thread): '\(value)'")
-  }
+    .subscribe(on: queue) // 事件的触发, 转移到了 queue 中尽心给调度了. 
+    .receive(on: DispatchQueue.main)
+    .sink { value in
+        let thread = Thread.current.number
+        print("Received computation result on thread \(thread): '\(value)'")
+    }
 
 
 //: [Next](@next)
 /*:
  Copyright (c) 2023 Kodeco Inc.
-
+ 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
-
+ 
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
-
+ 
  Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
  distribute, sublicense, create a derivative work, and/or sell copies of the
  Software in any work that is designed, intended, or marketed for pedagogical or
@@ -44,7 +44,7 @@ let subscription = computationPublisher
  or information technology.  Permission for such use, copying, modification,
  merger, publication, distribution, sublicensing, creation of derivative works,
  or sale is expressly withheld.
-
+ 
  This project and source code may use libraries or frameworks that are
  released under various Open-Source licenses. Use of those libraries and
  frameworks are governed by their own individual licenses.
